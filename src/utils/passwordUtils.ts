4c62b687cd22106fca8b5e62e9de860dad5237ab
HUNG NGO <hungngo1607@gmail.com>
2024-04-04 23:46:36 -0400

    adding authentication

---
+++ src/utils/passwordUtils.ts
@@ -0,0 +1,12 @@
+import bcrypt from 'bcrypt';
+
+export const hashPassword = async (password: string) => {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+};
+
+export const comparePassword = async (password: string, hashedPassword: string) => {
+    const isMatch = await bcrypt.compare(password, hashedPassword);
+    return isMatch;
+};
