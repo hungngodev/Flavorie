38969c2e0f175b63ce8f131ad48a67b1da49d835
viennguyen24 <viennguyen@usf.edu>
2024-04-08 00:07:02 -0400

    api call added to register

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -3,45 +3,100 @@ import {
   ChakraProvider,
   Flex,
   FormControl,
+  FormErrorMessage,
   Heading,
   Input,
   Link,
   VStack,
 } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
 
+interface FormFields {
+  username: string;
+  password: string;
+}
 const Login: React.FC = () => {
+  const onSubmit: SubmitHandler<FormFields> = (data) => {
+    //! submit logic added later
+    console.log(JSON.stringify(data));
+    console.log(errors);
+  };
+
+  const requiredErrorMessage = {
+    username: "You need a username to login",
+    password: "You need a password to login",
+  };
+  const minLengthErrorMessage = {
+    username: "Your username should have at least 4 characters",
+    password: "Your password should have at least 8 characters",
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormFields>();
   return (
     <ChakraProvider>
       <Flex height="100vh" alignItems="center" justifyContent="center">
         <VStack width="100%" marginInline="auto" spacing={8}>
           <Heading textAlign="center">Welcome back</Heading>
-          <FormControl
-            width={{
-              base: "95%",
-              md: "40%",
-            }}
-          >
+          <form onSubmit={handleSubmit(onSubmit)}>
             <VStack spacing={6}>
-              <Input
-                size="lg"
-                type="username"
-                placeholder="Enter username"
-                isRequired
-              />
-              <Input
-                size="lg"
-                type="password"
-                placeholder="Enter password"
-                isRequired
-              />
+              <FormControl isInvalid={errors.username ? true : false}>
+                <Input
+                  {...register("username", {
+                    required: {
+                      value: true,
+                      message: requiredErrorMessage["username"],
+                    },
+                    minLength: {
+                      value: 4,
+                      message: minLengthErrorMessage["username"],
+                    },
+                  })}
+                  size="lg"
+                  type="username"
+                  placeholder="Enter username"
+                  isRequired
+                />
+                <FormErrorMessage>
+                  {errors.username && errors.username.message}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={errors.password ? true : false}>
+                <Input
+                  {...register("password", {
+                    required: {
+                      value: true,
+                      message: requiredErrorMessage["password"],
+                    },
+                    minLength: {
+                      value: 8,
+                      message: minLengthErrorMessage["password"],
+                    },
+                  })}
+                  size="lg"
+                  type="password"
+                  placeholder="Enter password"
+                  isRequired
+                />
+                <FormErrorMessage>
+                  {errors.password && errors.password.message}
+                </FormErrorMessage>
+              </FormControl>
               <Link textAlign="left" href="#" alignSelf="flex-start">
                 Forgot your password?
               </Link>
-              <Button width="100%" colorScheme="teal">
+              <Button
+                width="100%"
+                colorScheme="teal"
+                type="submit"
+                isLoading={isSubmitting}
+              >
                 Login
               </Button>
             </VStack>
-          </FormControl>
+          </form>
           <Link textAlign="center" href="/register">
             Don't have an account?{" "}
             <span style={{ color: "teal" }}>Sign up now!</span>
--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -3,50 +3,173 @@ import {
   ChakraProvider,
   Flex,
   FormControl,
+  FormErrorMessage,
   Heading,
   Input,
   Link,
   VStack,
 } from "@chakra-ui/react";
-
+import { useEffect } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { redirect } from "react-router-dom";
+import customFetch from "../utils/customFetch";
+interface FormFields {
+  username: string;
+  email: string;
+  password: string;
+  reEnterPassword: string;
+}
+interface APIData {
+  name: string;
+  email: string;
+  password: string;
+}
 const Register: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormFields>();
+
+  const onSubmit: SubmitHandler<FormFields> = async (FormData) => {
+    // send data to back end and redirect to app
+    // redirect("/")
+    const newUserData: APIData = {
+      name: FormData.username,
+      email: FormData.email,
+      password: FormData.password,
+    };
+    console.log(newUserData);
+    let NewUserRequest;
+    try {
+      const NewUserRequest = await customFetch.post(
+        "/auth/register",
+        newUserData,
+        {
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        },
+      );
+      console.log(JSON.stringify(NewUserRequest.data));
+    } catch (error) {
+      console.log("this is error");
+      console.log(error);
+      return;
+    } finally {
+      redirect("/");
+    }
+  };
+  const minLengthErrorMessage = {
+    username: "Your username should have at least 4 characters",
+    password: "Your password should have at least 8 characters",
+    reEnterPassword:
+      "You need to re-enter your password with at least 8 characters",
+  };
+  const requiredErrorMessage = {
+    username: "You need a username to register",
+    email: "You need an email to register",
+    password: "You need a password to register",
+    reEnterPassword: "You need to re-enter your password correctly to register",
+  };
   return (
     <ChakraProvider>
       <Flex height="100vh" alignItems="center" justifyContent="center">
-        <VStack width="100%" marginInline="auto" spacing={8}>
+        <VStack marginInline="auto" spacing={8}>
           <Heading textAlign="center">
             Welcome to <span style={{ color: "teal" }}>Flavorie!</span>
           </Heading>
-          <FormControl
-            width={{
-              base: "95%",
-              md: "40%",
-            }}
-          >
+          <form onSubmit={handleSubmit(onSubmit)}>
             <VStack spacing={6}>
-              <Input
-                size="lg"
-                type="username"
-                placeholder="Enter username"
-                isRequired
-              />
-              <Input
-                size="lg"
-                type="password"
-                placeholder="Enter password"
-                isRequired
-              />
-              <Input
-                size="lg"
-                type="password"
-                placeholder="Re-enter password"
-                isRequired
-              />
-              <Button width="100%" colorScheme="teal">
-                Login
+              <FormControl isInvalid={errors.username ? true : false}>
+                <Input
+                  {...register("username", {
+                    minLength: {
+                      value: 4,
+                      message: minLengthErrorMessage["username"],
+                    },
+                    required: {
+                      value: true,
+                      message: requiredErrorMessage["username"],
+                    },
+                  })}
+                  size="lg"
+                  type="username"
+                  placeholder="Enter username"
+                  isRequired
+                />
+                <FormErrorMessage>
+                  {errors.username && errors.username.message}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={errors.email ? true : false}>
+                <Input
+                  {...register("email", {
+                    required: {
+                      value: true,
+                      message: requiredErrorMessage["email"],
+                    },
+                  })}
+                  size="lg"
+                  type="email"
+                  placeholder="Enter email"
+                  isRequired
+                />
+                <FormErrorMessage>
+                  {errors.email && errors.email.message}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={errors.password ? true : false}>
+                <Input
+                  {...register("password", {
+                    required: {
+                      value: true,
+                      message: requiredErrorMessage["password"],
+                    },
+                    minLength: {
+                      value: 8,
+                      message: minLengthErrorMessage["password"],
+                    },
+                  })}
+                  size="lg"
+                  type="password"
+                  placeholder="Enter password"
+                  isRequired
+                />
+                <FormErrorMessage>
+                  {errors.password && errors.password.message}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={errors.reEnterPassword ? true : false}>
+                <Input
+                  {...register("reEnterPassword", {
+                    required: {
+                      value: true,
+                      message: requiredErrorMessage["reEnterPassword"],
+                    },
+                    minLength: {
+                      value: 8,
+                      message: minLengthErrorMessage["reEnterPassword"],
+                    },
+                  })}
+                  size="lg"
+                  type="password"
+                  placeholder="Re-enter password"
+                  isRequired
+                />
+                <FormErrorMessage>
+                  {errors.reEnterPassword && errors.reEnterPassword.message}
+                </FormErrorMessage>
+              </FormControl>
+              <Button
+                width="100%"
+                colorScheme="teal"
+                type="submit"
+                isLoading={isSubmitting}
+                loadingText="Signing you up..."
+              >
+                Sign Up
               </Button>
             </VStack>
-          </FormControl>
+          </form>
           <Link textAlign="center" href="/login">
             Already have an account?{" "}
             <span style={{ color: "teal" }}>Sign in!</span>
