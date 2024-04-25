c050bb6f4163cba51e9b9f0df10e898a9dd958f0
HUNG NGO <hungngo1607@gmail.com>
2024-04-25 12:49:18 -0400

    adding utils cn

--- client/src/pages/main.tsx
+++ client/src/pages/main.tsx
@@ -1,11 +1,17 @@
+import Lottie from 'lottie-react';
 import React from 'react';
+import cooking1Animation from '../assets/animations/cooking1.json';
+import cooking2Animation from '../assets/animations/cooking2.json';
 import { useAuth } from '../hooks/';
+
 const Main: React.FC = () => {
   const auth = useAuth();
   return (
     <div>
       {auth.currentUser.username && <h1>Welcome {auth.currentUser.username}</h1>}
       <div>haihih</div>
+      <Lottie animationData={cooking1Animation} loop={true} style={{ height: 300 }} />
+      <Lottie animationData={cooking2Animation} loop={true} style={{ height: 300 }} />
     </div>
   );
 };
