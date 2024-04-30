eae785b1484e596da654dfad11f4658d165cd841
thuyvy119 <thuyvy11998@gmail.com>
2024-04-30 16:05:10 -0400

    Add user profile card

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
 import { ChakraBaseProvider, theme as chakraTheme, extendBaseTheme } from '@chakra-ui/react';
 import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 //import ImageSlide from "./components/ImageSlide";
-import ImageCard from './components/ImageCard';
+//import ImageCard from './components/ImageCard';
+import UserCard from './components/UserInforCard';
 
 const { Button } = chakraTheme.components;
 
@@ -37,17 +38,32 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  const imageProps = {
-    src: "../public/images/baked-brie-with-roasted-mushrooms.webp",
-    alt: "Baked brie with roasted mushroom",
-    description: "Baked brie cheese with roasted mushroom on top.",
-    borderRadius: '8px',
-    price: '$4.8'
+  // const imageProps = {
+  //   src: "../public/images/baked-brie-with-roasted-mushrooms.webp",
+  //   alt: "Baked brie with roasted mushroom",
+  //   description: "Baked brie cheese with roasted mushroom on top.",
+  //   borderRadius: '8px',
+  //   price: '$4.8'
+  // }
+  const userInfoProps = {
+    avatar: {
+            src: "../public/images/1989-Taylors-Version.webp",
+            username: "Taylor Swift",
+        },
+        email: "taylorswift@gmail.com",
+        phone: "+1 (202) 444 1989",
+        address: {
+            city: "Nashville",
+            state: "Tennessee",
+            country: "USA",
+            zipcode: "37208",
+        },
   }
   return (
     <ChakraBaseProvider theme={theme}>
       <RouterProvider router={router} />
-      <ImageCard imageProps={imageProps} />
+      {/* <ImageCard imageProps={imageProps} /> */}
+      <UserCard userInfoProps={userInfoProps} />
     </ChakraBaseProvider>
   );
 }
---
+++ client/src/components/UserInforCard.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import {
+    Avatar,
+    Box,
+    Button,
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    ChakraProvider,
+    Divider,
+    extendTheme,
+    Flex,
+    IconButton,
+    Heading,
+    HStack,
+    Stack,
+    Text,
+} from '@chakra-ui/react';
+import { EditIcon } from '@chakra-ui/icons'
+import theme from "../style/theme.tsx";
+
+interface UserProps {
+    userInfoProps: {
+        avatar: {
+            src: string;
+            username: string;
+        };
+        email: string;
+        phone: string;
+        address: {
+            city: string;
+            state: string;
+            country: string;
+            zipcode?: string;
+        };
+    };
+}
+
+const UserCard: React.FC<UserProps> = ({ userInfoProps }) => {
+    return (
+        <ChakraProvider theme={extendTheme(theme)}>
+        <Card maxW='md'>
+            <Flex ml='4' mt='4' mr='4' mb='4'>
+                <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
+                    <HStack>
+                        <Avatar size='2xl' name={userInfoProps.avatar.username} src={userInfoProps.avatar.src} />
+                        <Box ml='4'>
+                            <Heading size='md' fontWeight='bold'>{userInfoProps.avatar.username}</Heading>
+                            <Text>{userInfoProps.address.city}, {userInfoProps.address.state}</Text>
+                        </Box>
+                    </HStack>
+                </Flex>
+                <IconButton
+                    variant="solid"
+                    colorScheme="gray"
+                    aria-label='Edit'
+                    icon={<EditIcon />} 
+                />
+            </Flex>
+            <Box ml='36' mr='4'>
+                <Divider width="100%" borderColor='base.200' />
+            </Box>
+            <CardBody>
+                <HStack spacing='20px' ml='19' >
+                    <Box w='80px'>
+                        <Text color='base.300'>Email</Text>
+                        <Text color='base.300'>Phone</Text>
+                        <Text color='base.300'>City</Text>
+                        <Text color='base.300'>State</Text>
+                        <Text color='base.300'>Country</Text>
+                        <Text color='base.300'>Zipcode</Text>
+                    </Box>
+                    <Box w='240px'>
+                        <Text>{userInfoProps.email}</Text>
+                        <Text>{userInfoProps.phone}</Text>
+                        <Text>{userInfoProps.address.city}</Text>
+                        <Text>{userInfoProps.address.state}</Text>
+                        <Text>{userInfoProps.address.country}</Text>
+                        <Text>{userInfoProps.address.zipcode}</Text>
+                    </Box>
+                </HStack>
+            </CardBody>
+        </Card>
+        </ChakraProvider>
+    );
+};
+
+export default UserCard;
