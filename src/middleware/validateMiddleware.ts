a12476bf7a022c5f8dc42a10df566096d2b79501
thuyvy119 <thuyvy11998@gmail.com>
2024-05-10 00:38:02 -0400

    add validate register

--- src/middleware/validateMiddleware.ts
+++ src/middleware/validateMiddleware.ts
@@ -16,3 +16,51 @@ export const validateLoginInput = (req: Request, res: Response, next: NextFuncti
     }
 
 }
+
+export const registerData = z.object({
+    username: z.string().min(4),
+    email: z.string().email(),
+    password: z.string().min(6),
+    reEnterPassword: z.string().min(6),
+    });                                             
+
+export const validateRegisterInput = (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+    ) => {
+    try {
+        const data = registerData.parse(req.body);
+        if (data.password !== data.reEnterPassword) {
+            throw new BadRequestError("Please re-enter your password correctly");
+        }
+        next();
+    } 
+        catch (error) {
+        console.error("Error caught in validateRegisterInput:", error);
+            throw new BadRequestError('Password does not match');
+        } 
+};
+
+// mock data
+const sampleRegis = {
+    username: "Sophie",
+    email: "sophie.abc@gmail.com",
+    password: "12345678",
+    reEnterPassword: "12345605",
+};
+
+console.log(sampleRegis);
+const mockRequest = { body: sampleRegis } as Request;
+const mockResponse = {} as Response;
+const mockNext: NextFunction = (error?: any) => {
+    if (error) {
+        console.error("Error caught in validateRegisterInput:", error);
+        return; 
+    }
+    console.log('Registration successful'); 
+};
+
+validateRegisterInput(mockRequest, mockResponse, mockNext);
+
+
