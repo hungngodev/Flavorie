db9c23094aed8708bc1e032153c0113a8e590452
ANHTRAN <tran36a@mtholyoke.edu>
2024-05-10 23:09:19 -0400

    test

--- client/src/pages/Meal.tsx
+++ client/src/pages/Meal.tsx
@@ -1,8 +1,15 @@
+import { Specialty } from "../components/meals/Specialty";
 import { SearchBar } from "../components/ingredients/SearchBar";
+import { Flex, HStack, VStack } from "@chakra-ui/react";
 
 function Meal() {
   return ( 
+    <VStack spacing={4}>
+    <Specialty />
     <SearchBar />
+    </VStack>
+      
+    
   )
 }
 
