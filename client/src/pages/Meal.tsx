a682c183f1b9c0e6f2beef7405af9dfeb1f368b1
ANHTRAN <tran36a@mtholyoke.edu>
2024-05-13 20:49:38 -0400

    today-specialty

--- client/src/pages/Meal.tsx
+++ client/src/pages/Meal.tsx
@@ -1,10 +1,14 @@
 import { SearchBar } from "../components/ingredients/SearchBar";
+import { Specialty } from "../components/meals/Specialty";
 
 function Meal() {
   return ( 
+    <>
+    <Specialty />
     <SearchBar />
-      
+    </>
     
+      
   )
 }
 
