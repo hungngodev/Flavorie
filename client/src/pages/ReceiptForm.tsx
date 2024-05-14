63cfe8b7acd2857738be1d8248355519fbc625b4
viennguyen24 <viennguyen@usf.edu>
2024-05-14 09:54:03 -0400

    fixed form submit type errors, added state for receipts and update ui to show state changes

--- client/src/pages/ReceiptForm.tsx
+++ client/src/pages/ReceiptForm.tsx
@@ -13,119 +13,136 @@ import {
 } from '@chakra-ui/react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import React, { useState } from 'react';
-import { Controller, useFieldArray, useForm } from 'react-hook-form';
+import { Controller, SubmitHandler, useFieldArray, useForm } from 'react-hook-form';
 import { LuTrash } from 'react-icons/lu';
 import { z } from 'zod';
-import { mockReceipts } from './mockReceipt';
 
-const pricingSchema = z.string().refine(
-  (val) => {
-    const pattern = /^\$\d+\.\d{2}$/;
-    const isValidFormat = pattern.test(val);
-
-    const price = parseFloat(val.slice(1));
-    const isValidPrice = price >= 0.01;
-
-    return isValidFormat && isValidPrice;
-  },
-  { message: 'Expected price format of $x.xx, with a minimum price of $0.01' },
-);
-const quantitySchema = z.string().refine(
-  (val) => {
-    const price = parseInt(val);
-    return price >= 1;
-  },
-  { message: 'You should have at least one product' },
-);
+// ! Finish state prop
+// ! Implement UI
+// ! Implement Responsiveness
+// ! Implement accessibility
 
 const Receipt = z
   .object({
     name: z.string(),
-    price: pricingSchema,
-    quantity: quantitySchema,
+    quantity: z.number().min(0, { message: 'Quantity should be positive' }),
+    price: z.coerce.number().min(0, { message: 'Receipt price should be positive' }),
   })
-  .strict();
+  .strict()
+  .required({ name: true, quantity: true, price: true });
 
-const ReceiptRequired = Receipt.required({ name: true, price: true, quantity: true });
-type ReceiptType = z.infer<typeof ReceiptRequired>;
+const ReceiptField = z.object({
+  receipts: z.array(Receipt.required()),
+});
+
+type ReceiptFieldType = z.infer<typeof ReceiptField>;
 
 const ReceiptForm: React.FC = () => {
-  const { control, handleSubmit } = useForm({
-    resolver: zodResolver(ReceiptRequired),
+  const {
+    control,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(ReceiptField),
     defaultValues: {
-      receipts: [{ name: '', price: '', quantity: '' }], // the receipts field is an array of objects
+      receipts: [{ name: '', quantity: 0, price: 0.0 }], // the receipts field is an array of objects
     },
   });
 
-  const { fields, append, remove } = useFieldArray({
-    control: control, // hook the field array to react hook form
-    name: 'receipts', // name of the field that is an array in the form
-  });
+  const watchField = watch('receipts');
 
-  const submitReceipts = (data: any) => {
-    console.log(data);
+  const { fields, append, remove } = useFieldArray({
+    control: control,
+    name: 'receipts',
+  });
+  const [receipts, setReceipts] = useState<ReceiptFieldType>({ receipts: [{ name: '', quantity: 0, price: 0.0 }] });
+
+  const submitReceipts: SubmitHandler<ReceiptFieldType> = (receipts) => {
+    setReceipts(receipts);
+    console.log(receipts);
   };
-  const [receipts, setReceipts] = useState<ReceiptType[]>(mockReceipts.receipts);
 
   return (
-    <form onSubmit={handleSubmit(submitReceipts)}>
-      <HStack width="100%" justify="between">
-        <VStack spacing={4} justifySelf="start">
-          {fields.map((field, index) => (
-            <HStack key={field.id}>
-              <Controller
-                render={({ field: { ref, ...fieldProps } }) => <Input {...fieldProps} ref={ref} />}
-                control={control}
-                name={`receipts.${index}.name`}
-              />
-              <Controller
-                render={({ field: { ref, ...fieldProps } }) => (
-                  <NumberInput>
-                    <NumberInputField {...fieldProps} ref={ref} />
-                    <NumberInputStepper>
-                      <NumberIncrementStepper />
-                      <NumberDecrementStepper />
-                    </NumberInputStepper>
-                  </NumberInput>
-                )}
-                control={control}
-                name={`receipts.${index}.quantity`}
-              />
-              <Controller
-                render={({ field: { ref, ...fieldProps } }) => (
-                  <NumberInput>
-                    <NumberInputField {...fieldProps} ref={ref} />
-                    <NumberInputStepper>
-                      <NumberIncrementStepper />
-                      <NumberDecrementStepper />
-                    </NumberInputStepper>
-                  </NumberInput>
-                )}
-                control={control}
-                name={`receipts.${index}.price`}
-              />
-              <Button onClick={() => remove(index)}>
-                <LuTrash />
-              </Button>
-            </HStack>
-          ))}
-          <Button onClick={() => append({ name: '', quantity: '1', price: '0.00' })}>Add new receipt</Button>
-        </VStack>
-        <VStack spacing={4} justifySelf="end">
-          <Heading>Order summary</Heading>
-          <HStack justifyContent="between">
-            <Text>Total items</Text>
-            <Text>{0}</Text>
+    <form
+      onSubmit={handleSubmit(submitReceipts)}
+      style={{ width: '100%', display: 'flex', paddingTop: '3rem', justifyContent: 'between' }}
+    >
+      <VStack spacing={4} justifySelf="start">
+        {fields.map((field, index) => (
+          <HStack key={field.id}>
+            <Controller
+              control={control}
+              name={`receipts.${index}.name`}
+              render={({ field: { ref, ...fieldProps } }) => <Input {...fieldProps} placeholder="Enter your item" />}
+            />
+            <Controller
+              control={control}
+              name={`receipts.${index}.quantity`}
+              render={({ field: { onChange, ...fieldProps } }) => (
+                <NumberInput
+                  {...fieldProps}
+                  min={0}
+                  keepWithinRange={true}
+                  onChange={(valueNumber) => onChange(valueNumber)}
+                >
+                  <NumberInputField {...fieldProps} />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
+              )}
+            />
+            <Controller
+              control={control}
+              name={`receipts.${index}.price`}
+              render={({ field: { onChange, ...fieldProps } }) => (
+                <NumberInput
+                  precision={2}
+                  step={0.01}
+                  {...fieldProps}
+                  min={0.0}
+                  keepWithinRange={true}
+                  onChange={(valueNumber) => onChange(valueNumber)}
+                >
+                  <NumberInputField {...fieldProps} />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
+              )}
+            />
+            <Button onClick={() => remove(index)}>
+              <LuTrash />
+            </Button>
           </HStack>
-          <HStack justifyContent="between">
-            <Text>Total price</Text>
-            <Text>$12.25</Text>
-          </HStack>
-          <Button type="submit" onClick={handleSubmit(submitReceipts)}>
-            Submit changes
-          </Button>
-        </VStack>
-      </HStack>
+        ))}
+        <Button onClick={() => append({ name: '', quantity: 0, price: 0.0 })}>Add new receipt</Button>
+      </VStack>
+      <VStack spacing={4} justifySelf="end">
+        <Heading>Order summary</Heading>
+        <HStack justifyContent="between">
+          <Text>Total items</Text>
+          <Text>{fields.length}</Text>
+        </HStack>
+        <HStack justifyContent="between">
+          <Text>Total price</Text>
+          <Text>
+            {parseFloat(
+              watchField
+                .reduce((accumulator, field) => {
+                  return accumulator + field.price * field.quantity;
+                }, 0)
+                .toFixed(2),
+            )}
+          </Text>
+        </HStack>
+        <Button type="submit" onClick={() => console.log(fields)}>
+          Submit changes
+        </Button>
+      </VStack>
     </form>
   );
 };
