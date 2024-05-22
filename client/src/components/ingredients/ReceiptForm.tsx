97387989035f1931405bd51b030917c5fb8148d2
Vien Nguyen <viennguyen@usf.edu>
2024-05-22 20:36:11 +0000

    removed unecessary state, fixed bugs about adding / deleting and submitting form

--- client/src/components/ingredients/ReceiptForm.tsx
+++ client/src/components/ingredients/ReceiptForm.tsx
@@ -1,8 +1,13 @@
 import {
   Button,
+  Divider,
   HStack,
   Heading,
   Input,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
   NumberDecrementStepper,
   NumberIncrementStepper,
   NumberInput,
@@ -10,24 +15,13 @@ import {
   NumberInputStepper,
   Text,
   VStack,
-  Divider,
-  Select,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuItemOption,
-  MenuGroup,
-  MenuOptionGroup,
-  MenuDivider,
 } from '@chakra-ui/react';
 import { zodResolver } from '@hookform/resolvers/zod';
-import React, { useState, useEffect } from 'react';
-import { Controller, useFieldArray, useForm, SubmitHandler } from 'react-hook-form';
+import { Focus, PenLine, Search, Trash, X } from 'lucide-react';
+import React, { useEffect } from 'react';
+import { Controller, SubmitHandler, useFieldArray, useForm } from 'react-hook-form';
 import { z } from 'zod';
-import { Trash, Focus, Search, ChevronDown, X } from 'lucide-react';
-import { PenLine } from 'lucide-react';
-import mockReceipts from "./mockReceipts"
+import mockReceipts from './mockReceipts';
 
 const Receipt = z
   .object({
@@ -48,36 +42,35 @@ const ReceiptField = z.object({
 
 type ReceiptFieldType = z.infer<typeof ReceiptField>;
 
-
 const ReceiptForm: React.FC = () => {
-  const [receiptData, setReceiptData] = useState<ReceiptFieldType>({
-    receipts: 
-    mockReceipts.data?.map((item: any) => ({
-        name: item.name,
-        quantity: item.quantity.toString(),
-        price: item.price.replace('$', ''),
-        suggested: {
-          display: false,
-          items: item["potential_matches"]
-        }
-    })) 
-    ?? 
-    [{ name: 'test', quantity: "0", price: "0.0", suggested: {display: false, items: [""]}}],
-  });
-
   const {
     control,
     handleSubmit,
     watch,
+    setValue,
+    getValues,
+    reset,
     formState: { errors },
-  } = useForm({
+  } = useForm<ReceiptFieldType>({
     resolver: zodResolver(ReceiptField),
     defaultValues: {
-      receipts: receiptData.receipts.length > 0
-        ? receiptData.receipts
-        : [{ name: '', quantity: "0", price: "0.0", suggested: { display: false, items: [""] } }],
+      receipts:
+        mockReceipts?.data?.length > 0
+          ? mockReceipts.data.map((receipt: any) => {
+              return {
+                name: receipt.name.toLowerCase(),
+                quantity: receipt.quantity,
+                price: receipt.price,
+                suggested: {
+                  display: false,
+                  items: receipt['potential_matches'] ?? [''],
+                },
+              };
+            })
+          : [{ name: '', quantity: '0', price: '0.0', suggested: { display: false, items: [''] } }],
     },
   });
+
   const watchField = watch('receipts');
 
   const { fields, append, remove } = useFieldArray({
@@ -85,40 +78,46 @@ const ReceiptForm: React.FC = () => {
     name: 'receipts',
   });
   useEffect(() => {
-    console.log(fields)
-    console.log(receiptData.receipts)
-  }, [fields, receiptData])
-
+    console.log(fields);
+  }, [fields]);
 
   const submitReceipts: SubmitHandler<ReceiptFieldType> = (receiptResponse) => {
-    setReceiptData(() => {
-      const updatedReceipts = [...receiptResponse.receipts];
-      return {receipts: updatedReceipts};
-    });
+    console.log(receiptResponse);
   };
 
   return (
     <form
       onSubmit={handleSubmit(submitReceipts)}
       style={{
-        gap: "1.25rem",
-        overflow: "auto",
-        height: "100dvh",
+        gap: '1.25rem',
+        overflow: 'auto',
+        height: '100dvh',
         display: 'flex',
         paddingBlock: '2.75rem',
-        paddingInline: "2rem",
+        paddingInline: '2rem',
         justifyContent: 'space-evenly',
-        width: "100%",
-        backgroundColor: "#F7FAFC",
-        flexWrap: "wrap"
+        width: '100%',
+        backgroundColor: '#F7FAFC',
+        flexWrap: 'wrap',
       }}
     >
-      <VStack flex={4} backgroundColor="whiteAlpha.700" spacing={2} alignItems="flex-start" justifySelf="start" boxShadow='base' height="fit-content" rounded='md' paddingBlock={3} paddingInline={6}>
+      <VStack
+        flex={4}
+        backgroundColor="whiteAlpha.700"
+        spacing={2}
+        alignItems="flex-start"
+        justifySelf="start"
+        boxShadow="base"
+        height="fit-content"
+        rounded="md"
+        paddingBlock={3}
+        paddingInline={6}
+      >
         <Heading fontSize="3xl" color="teal" fontWeight="semibold" alignSelf="start">
           Items
         </Heading>
         {fields.map((field, index) => (
-          <HStack width="100%" marginBlock={14}>
+          <HStack width="100%" marginBlock={14} key={field.id}>
             <VStack width="100%" alignItems="start">
               <HStack>
                 <Controller
@@ -127,30 +126,35 @@ const ReceiptForm: React.FC = () => {
                   render={({ field: { ref, ...fieldProps } }) => (
                     <VStack width="fit-content" alignItems="start">
                       <Text>Name</Text>
-                      <Input borderRadius="md" paddingX="0.95em" {...fieldProps} placeholder="Enter your item" variant="flushed" onChange={(val) => fieldProps.onChange(val)} />
-                      {
-                        receiptData.receipts[index]?.suggested?.display && (
-                          <Menu>
-                          <MenuButton
-                            as={Button}
-                            arial-label="options"
-                            rightIcon={<Search />}
-                          >
-                          Suggestions
+                      <Input
+                        borderRadius="md"
+                        paddingX="0.95em"
+                        {...fieldProps}
+                        placeholder="Enter your item"
+                        variant="flushed"
+                        onChange={(val) => fieldProps.onChange(val)}
+                      />
+                      {fields[index].suggested.display && (
+                        <Menu>
+                          <MenuButton as={Button} arial-label="options" rightIcon={<Search />}>
+                            Suggestions
                           </MenuButton>
                           <MenuList>
-                          {receiptData.receipts[index]?.suggested?.items.map((suggest) => {
-                            return (
-                              <MenuItem key={suggest} onClick={() => {
-                                fieldProps.onChange(suggest);
-                                }}
-                              >{suggest}</MenuItem>
-                            )
-                          })}
+                            {fields[index].suggested.items.map((suggest: string) => {
+                              return (
+                                <MenuItem
+                                  key={suggest}
+                                  onClick={() => {
+                                    fieldProps.onChange(suggest);
+                                  }}
+                                >
+                                  {suggest}
+                                </MenuItem>
+                              );
+                            })}
                           </MenuList>
                         </Menu>
-                        )
-                      }
+                      )}
                     </VStack>
                   )}
                 />
@@ -207,78 +211,90 @@ const ReceiptForm: React.FC = () => {
               </HStack>
             </VStack>
             <VStack alignSelf="stretch" justifyContent="space-between">
-              <Text alignSelf="end" fontSize="xl" fontWeight="semibold" color="blackAlpha.900">${(parseFloat(watchField[index].price) * parseFloat(watchField[index].quantity)).toFixed(2) || 0}</Text>
+              <Text alignSelf="end" fontSize="xl" fontWeight="semibold" color="blackAlpha.900">
+                ${(parseFloat(watchField[index].price) * parseFloat(watchField[index].quantity)).toFixed(2) || 0}
+              </Text>
               <HStack>
-                <Button color="blackAlpha.700" letterSpacing="-0.005em" fontWeight="semibold" leftIcon={<Trash />} onClick={() => { 
-                  setReceiptData((prevReceipt) => {
-                    const currentReceipts = [...prevReceipt.receipts];
-                    currentReceipts.splice(index, 1);
-                    return {receipts: currentReceipts}
-                  })
-                  remove(index); 
-                  console.log(fields);
-                }} variant="ghost">
+                <Button
+                  color="blackAlpha.700"
+                  letterSpacing="-0.005em"
+                  fontWeight="semibold"
+                  leftIcon={<Trash />}
+                  onClick={() => {
+                    remove(index);
+                    console.log(fields);
+                  }}
+                  variant="ghost"
+                >
                   Delete
                 </Button>
-                <Button color="blackAlpha.700" letterSpacing="-0.005em" fontWeight="semibold" leftIcon={receiptData.receipts[index].suggested?.display ? <X/> : <PenLine />} variant="ghost"
+                <Button
+                  color="blackAlpha.700"
+                  letterSpacing="-0.005em"
+                  fontWeight="semibold"
+                  leftIcon={fields[index].suggested.display ? <X /> : <PenLine />}
+                  variant="ghost"
                   onClick={() => {
-                    setReceiptData((prevReceipts) => {
-                      const updatedReceipts = [...prevReceipts.receipts]
-                      updatedReceipts[index] = {
-                        ...updatedReceipts[index],
-                        suggested:{
-                          display: !updatedReceipts[index].suggested.display,
-                          items: [...updatedReceipts[index].suggested.items],
-                        }
-                      }
-                      return {receipts: updatedReceipts};
-                    });
+                    const updatedFields = [...fields];
+                    updatedFields[index].suggested.display = !updatedFields[index].suggested.display;
+                    reset({ receipts: updatedFields });
                   }}
                 >
-                  {receiptData.receipts[index]?.suggested?.display ? "Close" : "Update"}
+                  {fields[index].suggested.display ? 'Close' : 'Update'}
                 </Button>
               </HStack>
             </VStack>
           </HStack>
         ))}
-       <Button
-  colorScheme="teal"
-  letterSpacing="-0.005em"
-  fontWeight="semibold"
-  onClick={() => {
-    setReceiptData((prevReceiptData) => ({
-        ...prevReceiptData,
-        receipts: [...prevReceiptData.receipts, {
-            name: '',
-            quantity: "0",
-            price: "0.0",
-            suggested: { display: false, items: [""] },
-        }],
-    }));
-    append({
-        name: '',
-        quantity: "0",
-        price: "0.0",
-        suggested: { display: false, items: [""] },
-    });
-}}
->
-  Add new receipt
-</Button>
+        <Button
+          colorScheme="teal"
+          letterSpacing="-0.005em"
+          fontWeight="semibold"
+          onClick={() => {
+            append({
+              name: '',
+              quantity: '0',
+              price: '0.0',
+              suggested: { display: false, items: [''] },
+            });
+          }}
+        >
+          Add new receipt
+        </Button>
       </VStack>
 
-      <VStack backgroundColor="whiteAlpha.700" spacing={6} alignItems="start" position="sticky" top={0} boxShadow='base' rounded="md" height="fit-content" paddingBlock={6} paddingInline={8} flex={1} minWidth="35%">
-        <Heading fontSize="2xl" color="teal" fontWeight="semibold">Order summary</Heading>
+      <VStack
+        backgroundColor="whiteAlpha.700"
+        spacing={6}
+        alignItems="start"
+        position="sticky"
+        top={0}
+        boxShadow="base"
+        rounded="md"
+        height="fit-content"
+        paddingBlock={6}
+        paddingInline={8}
+        flex={1}
+        minWidth="35%"
+      >
+        <Heading fontSize="2xl" color="teal" fontWeight="semibold">
+          Order summary
+        </Heading>
         <Divider />
 
         <HStack width="100%" justifyContent="space-between">
-          <Text fontWeight="semibold" color="blackAlpha.700">Total items</Text>
+          <Text fontWeight="semibold" color="blackAlpha.700">
+            Total items
+          </Text>
           <Text color="blackAlpha.600">{fields.length}</Text>
         </HStack>
         <HStack justifyContent="space-between" width="100%">
-          <Text fontWeight="semibold" color="blackAlpha.700">Total price</Text>
+          <Text fontWeight="semibold" color="blackAlpha.700">
+            Total price
+          </Text>
           <Text color="blackAlpha.600">
-            $ {parseFloat(
+            ${' '}
+            {parseFloat(
               watchField
                 .reduce((accumulator, field) => {
                   return accumulator + parseFloat(field.price) * parseFloat(field.quantity);
@@ -290,7 +306,7 @@ const ReceiptForm: React.FC = () => {
 
         <Divider />
 
-        <Button letterSpacing="-0.005em" fontWeight="semibold" width="100%" type="submit" colorScheme='teal'>
+        <Button letterSpacing="-0.005em" fontWeight="semibold" width="100%" type="submit" colorScheme="teal">
           Submit changes
         </Button>
         <Button letterSpacing="-0.005em" fontWeight="semibold" width="100%" leftIcon={<Focus />} variant="outline">
