1064628442e56caa848f7d8ac41fd1e8f774b966
HUNG NGO <hungngo1607@gmail.com>
2024-05-22 00:58:11 -0400

    adding eslint

--- src/routes/mealRouter.ts
+++ src/routes/mealRouter.ts
@@ -2,7 +2,8 @@ import express from "express";
 import {
   getRanDomMealsAuthenticated,
   getRandomMealsUnauthenticated,
-  getAllMeals
+  getAllMeals,
+
 } from "../controllers/mealController.ts";
 import { getDietAndAllergy } from "../middleware/userMiddleware.ts";
 import { checkUser } from "../middleware/authMiddleware.ts";
@@ -10,5 +11,7 @@ import { catchAsync } from "../utils/catchAsync.ts";
 const router = express.Router();
 
 router.get("/", checkUser, getDietAndAllergy, catchAsync(getAllMeals));
+router.route("/:id")
+  .get(checkUser, catchAsync(getRanDomMealsAuthenticated))
 
 export default router;
