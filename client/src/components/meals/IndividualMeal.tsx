9c8213419592517f3dcca86b513c7a4372d5a507
thuyvy119 <thuyvy11998@gmail.com>
2024-05-22 22:44:49 -0500

    update-individual-meal

--- client/src/components/meals/IndividualMeal.tsx
+++ client/src/components/meals/IndividualMeal.tsx
@@ -1,10 +1,15 @@
-import { Box, Button, Flex, HStack, IconButton, VStack, Heading, Text } from '@chakra-ui/react';
+import { Box, ButtonGroup, Button, Flex, HStack, IconButton, VStack, Heading, Text } from '@chakra-ui/react';
+import { StarIcon, PrintIcon, ExternalLinkIcon } from '@chakra-ui/icons';
+import { FaShareAlt, FaStar, FaPrint } from 'react-icons/fa';
 import ImageSlide from "./ImageSlide";
 
 interface MealProps {
     individualMeal: Meal[];
-    title:string;
-    overview:string;
+    title: string;
+    overview: string;
+    totalTime: string;
+    servings: string;
+    calories: string;
 };
 
 interface Meal {
@@ -13,7 +18,7 @@ interface Meal {
     description: string;
 }
 
-function IndividualMeal({ individualMeal, title, overview }: MealProps) {
+function IndividualMeal({ individualMeal, title, overview, totalTime, servings, calories }: MealProps) {
     return (
         <Flex alignItems="center" justifyContent="center">
             <Box textAlign="center" mb={8}>
@@ -23,8 +28,27 @@ function IndividualMeal({ individualMeal, title, overview }: MealProps) {
                 <Text align="center" w="full" fontSize="28" mt={4}>
                     {overview}
                 </Text>
+                <HStack align="left" mt={4}></HStack>
             </Box>
+            <HStack justifyContent="flex-start" width="100%" mb={4}>
+                <Box bg="base.200">Total time: {totalTime}</Box>
+                <Box bg="base.200">Servings: {servings}</Box>
+                <Box bg="base.200">Calories per serving: {calories}</Box>
+            </HStack>
             <ImageSlide dishes={individualMeal} />
+            <HStack justifyContent="flex-end" width="100%" mt={4}>
+                <ButtonGroup spacing={4}>
+                    <Button rightIcon={<FaStar />} colorScheme="blue">
+                        Rate
+                    </Button>
+                    <Button rightIcon={<FaPrint />} colorScheme="blue">
+                        Print
+                    </Button>
+                    <Button rightIcon={<FaShareAlt />} colorScheme="blue">
+                        Share
+                    </Button>
+                </ButtonGroup>
+            </HStack>
         </Flex>
         );
 }
