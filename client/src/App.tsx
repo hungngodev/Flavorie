f732dcc5bbd62bfcfabf8a90cb3b68de425532bd
Vien Nguyen <viennguyen@usf.edu>
2024-05-30 13:40:56 +0000

    fixed bugs main file delete incoming change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,23 @@
 import { ChakraBaseProvider, extendTheme } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Slide, ToastContainer } from 'react-toastify';
-import HomeLayout from './layouts/HomeLayout';
-import { Ingredient, Login, Main, Meal, Register } from './pages/index';
+import HomeLayout from './layouts/HomeLayout.tsx';
+import { loader as ingredientsLoader } from './pages/Ingredient.tsx';
+import { loader as mealsLoader } from './pages/Meal.tsx';
+import { Ingredient, Login, Main, Meal, Register, User } from './pages/index';
 import theme from './style/theme';
+import IndividualMeal from './pages/Recipe.tsx';
+import { Dish } from '@/components/meals/ImageSlide'
 
-// const { Button } = chakraTheme.components;
-
-// const theme = extendBaseTheme({
-//   components: {
-//     Button,
-//   },
-// });
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 const router = createBrowserRouter([
   {
@@ -36,31 +42,99 @@ const router = createBrowserRouter([
         children: [
           {
             index: true,
-            element: <Ingredient />,
+            element: <div>HIHIH</div>,
           },
           {
             path: ':category',
             element: <Ingredient />,
+            loader: ingredientsLoader(queryClient),
           },
         ],
       },
       {
         path: 'meals',
-        element: <Meal />,
+        children: [
+          {
+            index: true,
+            element: <Meal />,
+            loader: mealsLoader(queryClient),
+          },
+          {
+            path: ':mealId',
+            element: <div>Meal</div>,
+          },
+        ],
       },
       {
         path: 'community',
         element: <div>Community</div>,
       },
+      {
+        path: 'profile',
+        element: <User />,
+      },
     ],
   },
 ]);
 
+const IndividualMealWrapper = () => {
+  const sampleMeal: Dish[] = [
+    {
+      image: '../public/images/baked-brie-with-roasted-mushrooms.webp',
+      title: 'Baked brie with roasted mushroom',
+      description: ' Step 1: Bake brie and roasted mushroom.',
+    },
+    {
+      image: '../public/images/apple-and-cheddar-crisp-salad-scaled.webp',
+      title: 'Apple and cheddar crisp salad',
+      description: 'Step 2: Wash salad and apple',
+    },
+    {
+      image: '../public/images/buffalo-chicken-cobb-salad-scaled.webp',
+      title: 'Buffalo chicken cobb salad',
+      description: 'Step 3: Roast buffalo chicken',
+    },
+    {
+      image: '../public/images/chocolate-raspberry-pavlova-stack-12-scaled.webp',
+      title: 'Chocolate raspberry pavlova stack',
+      description: 'Step 4: Wash raspberry',
+    },
+    {
+      image: '../public/images/new-york-crumb-cake-7-scaled.webp',
+      title: 'New york crumb cake',
+      description: 'Step 5: Bake cake',
+    },
+    {
+      image: '../public/images/summer-ricotta-grilled-vegetables.webp',
+      title: 'Summer ricotta grilled vegetables',
+      description: 'Step 6: Grilled vegetables after washing',
+    },
+  ];
+
+  return (
+    <IndividualMeal
+      individualMeal={sampleMeal}
+      title="Sample Meal Title"
+      overview="This is an overview of the sample meal."
+      totalTime="45 mins"
+      servings="4"
+      calories="500"
+      averageStar='5'
+      numReviews='1'
+    />
+  );
+};
+
 function App() {
   return (
     <ChakraBaseProvider theme={extendTheme(theme)}>
-      <RouterProvider router={router} />
-      <ToastContainer autoClose={5000} limit={3} transition={Slide} />
+      <QueryClientProvider client={queryClient}>
+      {/* <IndividualMealWrapper /> */}
+      {/* <ImageScan /> */}
+        <RouterProvider router={router} />
+        <ToastContainer autoClose={5000} limit={3} transition={Slide} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraBaseProvider>
   );
 }
