ee9a9ed46039d6a98c1535664a1e55abbe5a114d
Vien Nguyen <viennguyen@usf.edu>
2024-05-30 12:49:13 +0000

    save custom-inputs and register/login page

---
+++ client/src/components/form/CustomNumberInput.tsx
@@ -0,0 +1,46 @@
+import {
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputFieldProps,
+  NumberInputProps,
+  NumberInputStepper,
+} from '@chakra-ui/react';
+import React from 'react';
+import { ControllerRenderProps, FieldValues } from 'react-hook-form';
+
+interface CustomNumberInputProps<T extends FieldValues> extends NumberInputProps {
+  inputFieldProps?: NumberInputFieldProps;
+  isInValid?: boolean;
+  label?: string;
+  helperText?: string;
+  errorText?: string;
+  fieldProps: ControllerRenderProps<T, any>;
+}
+
+const InputNumber: React.ForwardRefRenderFunction<HTMLInputElement, CustomNumberInputProps<FieldValues>> = (
+  { label, helperText, errorText, isInValid, inputFieldProps, ...props },
+  forwardedRef,
+) => {
+  return (
+    <FormControl isInvalid={isInValid}>
+      <FormLabel>{label}</FormLabel>
+      <FormHelperText>{helperText}</FormHelperText>
+      <NumberInput {...props} ref={forwardedRef}>
+        <NumberInputField {...inputFieldProps} value={props.value} />
+        <NumberInputStepper>
+          <NumberIncrementStepper border="none" />
+          <NumberDecrementStepper border="none" />
+        </NumberInputStepper>
+      </NumberInput>
+      <FormErrorMessage>{errorText}</FormErrorMessage>
+    </FormControl>
+  );
+};
+
+export default React.forwardRef(InputNumber);
