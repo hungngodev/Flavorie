1c2e44e03a55dc7b6d8659ab889a0b061316054a
Vien Nguyen <viennguyen@usf.edu>
2024-06-02 23:27:15 +0000

    updated UI and default styling for CustomNumberInput

--- client/src/components/form/CustomNumberInput.tsx
+++ client/src/components/form/CustomNumberInput.tsx
@@ -13,9 +13,10 @@ import {
 } from '@chakra-ui/react';
 import React from 'react';
 import { ControllerRenderProps, FieldValues } from 'react-hook-form';
+import { Plus, Minus } from 'lucide-react';
 
 interface CustomNumberInputProps<T extends FieldValues> extends NumberInputProps {
-  inputFieldProps: NumberInputFieldProps;
+  inputFieldProps?: NumberInputFieldProps;
   isInValid?: boolean;
   label?: string;
   helperText?: string;
@@ -29,14 +30,25 @@ const InputNumber: React.ForwardRefRenderFunction<HTMLInputElement, CustomNumber
 ) => {
   return (
     <FormControl isInvalid={isInValid}>
-      <FormLabel>{label}</FormLabel>
+      <FormLabel color={props.color ?? "blackAlpha.600"}>{label}</FormLabel>
       <FormHelperText>{helperText}</FormHelperText>
-      <NumberInput {...props} ref={forwardedRef}>
-        <NumberInputField {...inputFieldProps} value={props.value} />
-        <NumberInputStepper>
-          <NumberIncrementStepper border="none" />
-          <NumberDecrementStepper border="none" />
-        </NumberInputStepper>
+      <NumberInput {...props} ref={forwardedRef} variant={props?.variant ?? "ghost"}
+        display={props.display ?? "flex"}
+        flexDirection={props.flexDirection ?? "row"}
+        border={props.border ?? '2px'}
+        borderColor={props.borderColor ?? 'gray.200'}
+        borderRadius={props.borderRadius ?? "2.55em"}
+        paddingX={props.paddingX ?? "0.5em"}
+        size={props.size ?? "sm"}
+        width={props.width ?? "min-content"}
+        minWidth={props.minWidth ?? "8.5em"}>
+        <NumberDecrementStepper border="none" children={<Minus />} />
+        <NumberInputField {...inputFieldProps} value={props.value}
+          textAlign="center"
+          padding={inputFieldProps?.padding ?? 0}
+          margin={inputFieldProps?.margin ?? 0}
+          color={props.color ?? "blackAlpha.700"} />
+        <NumberIncrementStepper border="none" children={<Plus />} />
       </NumberInput>
       <FormErrorMessage>{errorText}</FormErrorMessage>
     </FormControl>
