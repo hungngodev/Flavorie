2c5817faed5e54da7ec04961efaca35ed91c643b
viennguyen24 <viennguyen@usf.edu>
2024-06-09 07:11:17 +0000

    updated post create function

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -1,23 +1,26 @@
+
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import { buildPostDocument } from "../services/postServices.ts"
 import { ServerError } from "../errors/customErrors.ts";
+import { buildPostDocument } from "../services/postServices.ts";
 
 
 export const createPost = async (req: Request, res: Response) => {
-    try{
-        const info = req.body;
-        const newPost = buildPostDocument(info);
-        if(newPost) {
-            res.status(StatusCodes.OK).send(newPost);
-        }
-        else{
-            res.status(StatusCodes.NOT_ACCEPTABLE);
-        }
-        
-    } catch (err) {
-        console.log("here")
-        throw new ServerError(`${err}`)
+  try {
+    const info = req.body;
+    const newPost = await buildPostDocument(info);
+    if (!newPost) {
+      res.status(StatusCodes.NOT_ACCEPTABLE);
     }
-}
+    res.status(StatusCodes.OK).json({ id: newPost });
+  } catch (err) {
+    console.log("here");
+    throw new ServerError(`${err}`);
+  }
+};
+
+
+
+
+
 
--- src/services/postServices.ts
+++ src/services/postServices.ts
@@ -1,20 +1,27 @@
+
 import { ServerError, UnauthenticatedError } from "../errors/customErrors.ts";
 import PostModel, { Post } from "../models/Post.ts";
 import UserModel from "../models/UserModel.ts";
 
-export const buildPostDocument = async (post: Post) => {
+
+export const buildPostDocument = async (post: any): Promise<string> => {
   try {
-    const { author, id } = post;
-    const user = await UserModel.find(author);
-    const existedPost = await PostModel.findById(id);
+    const { author } = post;
+    const user = await UserModel.find({ _id: author });
+    const existedPost = await PostModel.find({ _id: post.id });
     if (!user) {
       throw new UnauthenticatedError(`invalid author`);
     }
-    if (existedPost) {
-      throw new ServerError(`post already existed`);
+    if (existedPost.length > 0) {
+      throw new ServerError(`post already exists`);
     }
     const newPost = await PostModel.create({
-      post,
+      _id: post.id,
+      id: post.id,
+      author: post.author,
+      header: post.header,
+      body: post.body,
+      privacy: post.privacy,
     });
     return newPost.id;
   } catch (error) {
