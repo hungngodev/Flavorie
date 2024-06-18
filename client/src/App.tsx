172ebb7d1ed59cd1941fe089126b4c2790d98bc1
thuyvy119 <thuyvy11998@gmail.com>
2024-06-18 17:59:33 -0500

    edit padding add date for review card

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,16 +5,16 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Slide, ToastContainer } from 'react-toastify';
 import HomeLayout from './layouts/HomeLayout';
-import { Ingredient, loader as ingredientsLoader } from './pages/Ingredient.tsx';
-import { loader as mealsLoader } from './pages/Meal.tsx';
-import { loader as recipeLoader } from './pages/Recipe';
-import { Ingredient, Login, Main, Meal, Recipe, Register, User } from './pages/index';
+// import { Ingredient, loader as ingredientsLoader } from './pages/Ingredient.tsx';
+// import { loader as mealsLoader } from './pages/Meal.tsx';
+// import { loader as recipeLoader } from './pages/Recipe';
+// import { Ingredient, Login, Main, Meal, Recipe, Register, User } from './pages/index';
 import theme from './style/theme';
 import IndividualMeal from './pages/Recipe';
 import { BackendData } from './components/meals/ImageSlide';
 import NutritionCard from './components/ingredients/NutritionCard';
 import { IngredientProps } from './components/ingredients/NutritionCard';
-import ReviewCard, { Review } from './components/community/reviewCard.tsx';
+import ReviewCard, { Review } from './components/community/reviewCard';
 import { identity } from 'lodash';
 
 export const queryClient = new QueryClient({
@@ -25,69 +25,69 @@ export const queryClient = new QueryClient({
   },
 });
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomeLayout />,
-    children: [
-      {
-        index: true,
-        element: <Main />,
-      },
-      {
-        path: 'register',
-        element: <Register />,
-      },
-      {
-        path: 'login',
-        element: <Login />,
-      },
-      {
-        path: 'ingredients',
-        element: <Outlet />,
-        children: [
-          {
-            index: true,
-            element: <div>HIHIH</div>,
-          },
-          {
-            path: ':category',
-            element: <Ingredient />,
-            loader: ingredientsLoader(queryClient),
-          },
-        ],
-      },
-      {
-        path: 'meals',
-        children: [
-          {
-            index: true,
-            element: <Meal />,
-            loader: mealsLoader(queryClient),
-          },
-          {
-            path: ':mealId',
-            element: <Recipe />,
-            loader: recipeLoader(queryClient),
-          },
-        ],
-      },
-      {
-        path: 'community',
-        children: [
-          {
-            index: true,
-            element: <div>Community</div>,
-          },
-        ],
-      },
-      {
-        path: 'profile',
-        element: <User />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <HomeLayout />,
+//     children: [
+//       {
+//         index: true,
+//         element: <Main />,
+//       },
+//       {
+//         path: 'register',
+//         element: <Register />,
+//       },
+//       {
+//         path: 'login',
+//         element: <Login />,
+//       },
+//       {
+//         path: 'ingredients',
+//         element: <Outlet />,
+//         children: [
+//           {
+//             index: true,
+//             element: <div>HIHIH</div>,
+//           },
+//           {
+//             path: ':category',
+//             element: <Ingredient />,
+//             loader: ingredientsLoader(queryClient),
+//           },
+//         ],
+//       },
+//       {
+//         path: 'meals',
+//         children: [
+//           {
+//             index: true,
+//             element: <Meal />,
+//             loader: mealsLoader(queryClient),
+//           },
+//           {
+//             path: ':mealId',
+//             element: <Recipe />,
+//             loader: recipeLoader(queryClient),
+//           },
+//         ],
+//       },
+//       {
+//         path: 'community',
+//         children: [
+//           {
+//             index: true,
+//             element: <div>Community</div>,
+//           },
+//         ],
+//       },
+//       {
+//         path: 'profile',
+//         element: <User />,
+//       },
+//     ],
+//   },
+// ]);
 
 const mockComments: Review[] = [
   {
@@ -97,6 +97,7 @@ const mockComments: Review[] = [
       src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0WU10MyuUxOw4QSiIEzWPZn3sfytEl7Z4RA&s',
     },
     content: 'Parent comment.',
+    timestamp: '2024-02-06T17:30:00Z',
     children: [
       {
         id: '002',
@@ -105,23 +106,46 @@ const mockComments: Review[] = [
           src: 'https://akns-images.eonline.com/eol_images/Entire_Site/2023013/rs_1024x759-230113124412-1024-Phineas-and-Ferb-perry.ct.jpg?fit=around%7C1024:759&output-quality=90&crop=1024:759;center,top',
         },
         content: 'First child comment',
+        timestamp: '2024-02-07T10:30:00Z',
         children: [],
       },
       {
         id: '003',
         author: {
-          username: 'Cal A',
-          src: 'https://facts.net/wp-content/uploads/2023/09/22-facts-about-bubbles-the-powerpuff-girls-1694408222.jpg',
+          username: 'Celine',
+          src: 'https://static.wikia.nocookie.net/meme-hell/images/6/69/Blossom.jpg/revision/latest?cb=20160306164114',
         },
         content: 'Second child comment',
+        timestamp: '2024-02-07T10:30:00Z',
+        children: [],
+      },
+      {
+        id: '004',
+        author: {
+          username: 'Jay M',
+          src: 'https://i.scdn.co/image/ab6761610000e5eb7ac3385e1033229ea480dc9d',
+        },
+        content: 'Third child comment',
+        timestamp: '2024-02-07T10:30:00Z',
+        children: [],
+      },
+      {
+        id: '005',
+        author: {
+          username: 'Calvin',
+          src: 'https://facts.net/wp-content/uploads/2023/09/22-facts-about-bubbles-the-powerpuff-girls-1694408222.jpg',
+        },
+        content: 'Forth child comment',
+        timestamp: '2024-02-08T12:45:00Z',
         children: [
           {
-            id: '004',
+            id: '006',
             author: {
               username: 'Denny W',
-              src: 'path_to_denny_avatar.jpg',
+              src: 'https://target.scene7.com/is/image/Target/GUEST_65ecb462-c4ed-4efc-9ec1-fd9a015ebe36?wid=488&hei=488&fmt=pjpeg',
             },
             content: 'Nested child comment',
+            timestamp: '2024-02-09T09:00:00Z',
             children: [],
           },
         ],
@@ -131,11 +155,15 @@ const mockComments: Review[] = [
 ];
 
 function App() {
+  const handleReply = (content: string, parentReviewId: string) => {
+    console.log(`Reply to ${parentReviewId}: ${content}`);
+  };
+  
   return (
     <ChakraBaseProvider theme={extendTheme(theme)}>
-      <QueryClientProvider client={queryClient}>
+      {/* <QueryClientProvider client={queryClient}> */}
         {/* <ImageScan /> */}
-        <RouterProvider router={router} />
+        {/* <RouterProvider router={router} /> */}
         {/* <IndividualMeal
           recipeData={backendData}
           calories='340 kcal'
@@ -143,14 +171,14 @@ function App() {
           numReviews="3"
         /> */}
         {/* <NutritionCard {...mockdata} /> */}
-        {/* <Box>
+        <Box>
           {mockComments.map((review) => (
-              <ReviewCard key={review.id} review={review} />
+            <ReviewCard key={review.id} review={review} onReply={handleReply} />
           ))}
-        </Box> */}
-      <ToastContainer autoClose={5000} limit={3} transition={Slide} />
-      <ReactQueryDevtools />
-      </QueryClientProvider>
+        </Box>
+        <ToastContainer autoClose={5000} limit={3} transition={Slide} />
+        {/* <ReactQueryDevtools /> */}
+      {/* </QueryClientProvider> */}
     </ChakraBaseProvider>
   );
 }
--- client/src/components/community/reviewCard.tsx
+++ client/src/components/community/reviewCard.tsx
@@ -1,44 +1,71 @@
-import React, { useState } from "react";
+import React, { Children, useState } from "react";
 import { Avatar, Box, Button, HStack, Text, VStack} from '@chakra-ui/react';
 import { PersonalProps } from "../users/InfoCard";
+// import ReviewForm from "../form/Review";
 
 export interface Review {
     id: string;
     author: PersonalProps['avatar'];
     content: string;
+    timestamp: string;
     children: Review[];    
 }
 
 interface ReviewCardProps {
     review: Review;
+    onReply: (content: string, parentReviewId: string) => void;
 }
 
-const ReviewCard: React.FC<ReviewCardProps> = ({ review }) => {
+const ReviewCard: React.FC<ReviewCardProps> = ({ review, onReply }) => {
     const [showReplies, setShowReplies] = useState(false);
+    const [reply, setReply] = useState(false);
+
+    const handleReply = (content: string) => {
+        onReply(content, review.id);
+        setReply(false);
+    };
 
     return (
-        <Box ml="5" mt="2" mb="2">
-            <HStack>
+        <Box ml="5" mt="2">
+            <HStack align= "start" mb="4">
                 <Avatar name={review.author.username} src={review.author.src} />
-                <Box borderWidth= "1px" p="2"borderRadius="lg" bg="base.50">
-                    <Text fontWeight="bold" mb="2">{review.author.username}</Text>
-                    <Text mb="2">{review.content}</Text>
-                </Box>
+                <VStack>
+                    <Box borderWidth= "1px" p="2"borderRadius="lg" bg="base.50" width="600px">
+                        <Text fontWeight="bold" mb="2">{review.author.username}</Text>
+                        <Text>{review.content}</Text>
+                    </Box>
+                    <HStack width="600px" justifyContent="space-between">
+                        <Text fontSize="10" color="gray.500">{new Date(review.timestamp).toLocaleString()}</Text>
+                        <Button size="sm" variant= "link" onClick={() => setReply(!reply)}>
+                            Reply
+                        </Button>
+                        {reply && (
+                            <Box mt="2">
+                                {/* <ReviewForm onSubmit={handleReply} /> */}
+                            </Box>
+                        )}
+                    </HStack>
+                </VStack>
             </HStack>
             {review.children.length > 0 && (
-                <Button size="sm"
-                variant="link"
-                onClick={() => setShowReplies(!showReplies)}
-                >
-                    {showReplies ? 'Hide replies': `Show replies (${review.children.length})`}
-                </Button>
-            )}
-            {showReplies && (
-                <VStack spacing="4" align="start" pl="4">
-                    {review.children.map((reply) => (
-                        <ReviewCard key={reply.id} review={reply} />
+                <Box ml="8">
+                    {review.children.slice(0, 2).map((childReview) => (
+                        <ReviewCard key={childReview.id} review={childReview} onReply={onReply} />
                     ))}
-                </VStack>
+                    {review.children.length > 2 && (
+                        <Box ml="20">
+                            <Button size="sm"
+                            variant="link"
+                            onClick={() => setShowReplies(!showReplies)}
+                            >
+                            {showReplies ? 'Hide replies': `Show all replies (${review.children.length})`}
+                            </Button>
+                        </Box>
+                    )}
+                    {showReplies && review.children.slice(2).map((childReview) => (
+                        <ReviewCard key={childReview.id} review={childReview} onReply={onReply} />
+                        ))}
+                </Box>
             )}
         </Box>
     )
