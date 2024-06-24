277ba020970967801d019a6b71076caacb0b0940
thuyvy119 <thuyvy11998@gmail.com>
2024-06-24 17:55:14 -0500

    add validate review middleware for review router

--- src/middleware/validateMiddleware.ts
+++ src/middleware/validateMiddleware.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response } from "express";
-import z from "zod";
+import z, { string } from "zod";
 import { BadRequestError } from "../errors/customErrors.ts";
 import ExpressError from "../utils/ExpressError";
+import mongoose from "mongoose";
 
 // login
 export const logInData = z.object({
@@ -46,7 +47,42 @@ export const validateRegisterInput = (
         } 
 };
 
+// reivew
+const ReviewSchema = z.object({
+    userId: z.string().refine(val => mongoose.Types.ObjectId.isValid(val), {
+        message: "Invalid userId",
+    }),
+    postId: z.string().refine(val => mongoose.Types.ObjectId.isValid(val), {
+        message: "Invalid postId",
+    }),
+    content: z.string().nonempty({ message: "Content is required" }),
+    timestamp: z.date().default(new Date()),
+    childrenReview: z.array(z.string().refine(val => mongoose.Types.ObjectId.isValid(val), {
+            message: "Invalid childrenReview id",
+        }),
+        ).optional(),
+    parentReview: z.string().nullable().refine(val => val === null || mongoose.Types.ObjectId.isValid(val), {
+        message: "Invalid parentReview id",
+        }).optional(),
+});
 
+export const validateReview = (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+    ) => {
+    try {
+        ReviewSchema.parse(req.body);
+        next();
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+        const msg = error.errors.map(err => err.message).join(",");
+        next(new ExpressError(msg, 400));
+        } else {
+        next(error);
+        }
+    }
+};
 
 // mock data for register
 // const sampleRegis = {
--- src/routes/reviewRouter.ts
+++ src/routes/reviewRouter.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { authenticateUser, authorizeReviewOwner} from "../middleware/authMiddleware.ts";
+import { validateReview } from "../middleware/validateMiddleware.ts";
 import { catchAsync } from "../utils/catchAsync.ts";
 import Review from "../models/Review.ts";
 import { createReview, updateReview, deleteReview } from "../controllers/reviewController.ts";    
 
 const router = Router();
 
-router.post('/:postId', authenticateUser, catchAsync(createReview));
-router.put('/:postId/:reviewId', authenticateUser, authorizeReviewOwner, catchAsync(updateReview));
+router.post('/:postId', authenticateUser, validateReview, catchAsync(createReview));
+router.put('/:postId/:reviewId', authenticateUser, authorizeReviewOwner, validateReview, catchAsync(updateReview));
 router.delete('/:postId/:reviewId', authenticateUser, authorizeReviewOwner, catchAsync(deleteReview));
 
 export default router;
