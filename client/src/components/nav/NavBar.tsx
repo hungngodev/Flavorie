55e2507f62378173e28adb227d57910a57b02ddc
viennguyen24 <viennguyen@usf.edu>
2024-06-30 23:49:14 -0400

    changed ui on navbar and revamp home page

--- client/src/components/nav/NavBar.tsx
+++ client/src/components/nav/NavBar.tsx
@@ -2,70 +2,68 @@ import { Box, useBreakpointValue, useColorModeValue, useTheme } from '@chakra-ui
 import { DesktopNav } from './DesktopNav';
 import { MobileNav } from './MobileNav';
 export interface NavItem {
-  label: string;
-  subLabel?: string;
-  children?: Array<NavItem>;
-  href?: string;
+    label: string;
+    subLabel?: string;
+    children?: Array<NavItem>;
+    href?: string;
 }
 
 const NAV_ITEMS: Array<NavItem> = [
-  { label: 'Home', href: '/' },
-  {
-    label: 'Ingredients',
-    href: '/ingredients',
-  },
-  {
-    label: 'Meals',
-    href: '/meals',
-  },
-  {
-    label: 'Community',
-    href: '/community',
-    children: [
-      {
-        label: 'Browse',
-        subLabel: 'Look what others are cooking',
-        href: '/',
-      },
-      {
-        label: 'Share',
-        subLabel: 'share your favorite recipe',
-        href: '/',
-      },
-      {label: 'Meeting',
-        subLabel: 'Cook with friends',
-        href: '/meeting',
-      }
-    ],
-  },
-  {
-    label: 'Upload Receipts',
-    href: '/upload-receipts',
-  },
+    { label: 'Home', href: '/' },
+    {
+        label: 'Ingredients',
+        href: '/ingredients',
+    },
+    {
+        label: 'Meals',
+        href: '/meals',
+    },
+    {
+        label: 'Community',
+        href: '/community',
+        children: [
+            {
+                label: 'Browse',
+                subLabel: 'Look what others are cooking',
+                href: '/',
+            },
+            {
+                label: 'Share',
+                subLabel: 'share your favorite recipe',
+                href: '/',
+            },
+            { label: 'Meeting', subLabel: 'Cook with friends', href: '/meeting' },
+        ],
+    },
+    {
+        label: 'Upload Receipts',
+        href: '/upload-receipts',
+    },
 ];
 
 export default function NavBar() {
-  const NavtoRender = useBreakpointValue({
-    base: <MobileNav NavItems={NAV_ITEMS} />,
-    md: <DesktopNav NavItems={NAV_ITEMS} />,
-  });
-  const theme = useTheme();
-  return (
-    <Box
-      position="sticky"
-      top="0"
-      zIndex={19}
-      width={'100%'}
-      bg={theme.colors.palette_blue}
-      color={'white'}
-      minH={'60px'}
-      py={{ base: 2 }}
-      px={{ base: 4 }}
-      borderBottom={1}
-      borderStyle={'solid'}
-      borderColor={useColorModeValue('gray.200', 'gray.900')}
-    >
-      {NavtoRender}
-    </Box>
-  );
+    const NavtoRender = useBreakpointValue({
+        base: <MobileNav NavItems={NAV_ITEMS} />,
+        md: <DesktopNav NavItems={NAV_ITEMS} />,
+    });
+    const theme = useTheme();
+    return (
+        <Box
+            position="sticky"
+            top="0"
+            zIndex={19}
+            width={'100%'}
+            bgColor="#fef9ff"
+            // color={'white'}
+            minH={'60px'}
+            py={{ base: 2 }}
+            px={{ base: 4 }}
+            borderBottom={1}
+            borderStyle={'solid'}
+            borderColor={useColorModeValue('gray.200', 'gray.900')}
+            boxShadow="md"
+        >
+            {NavtoRender}
+        </Box>
+    );
 }
