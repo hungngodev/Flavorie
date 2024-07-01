8b968f6d4c27773c59b802f847ee8a61b49f65cc
viennguyen24 <viennguyen@usf.edu>
2024-07-01 08:36:05 -0400

    updated image files and theme

--- client/src/style/theme.tsx
+++ client/src/style/theme.tsx
@@ -1,84 +1,87 @@
 import '@fontsource/reddit-mono'; // Defaults to weight 400
 import { FaUserCircle } from 'react-icons/fa';
 const theme = {
-  useSystemColorMode: true,
-  colors: {
-    base: {
-      50: '#eceff1',
-      100: '#cfd8dc',
-      200: '#b0bec5',
-      300: '#90a4ae',
-      400: '#78909c',
-      500: '#607d8b',
-      600: '#546e7a',
-      700: '#455a64',
-      800: '#37474f',
-      900: '#263238',
-      d100: '#E1CDC1',
-      d200: '#12181B',
-      d400: '#FFFFFF',
-      d700: '#080C0D',
+    useSystemColorMode: true,
+    colors: {
+        base: {
+            50: '#eceff1',
+            100: '#cfd8dc',
+            200: '#b0bec5',
+            300: '#90a4ae',
+            400: '#78909c',
+            500: '#607d8b',
+            600: '#546e7a',
+            700: '#455a64',
+            800: '#37474f',
+            900: '#263238',
+            d100: '#E1CDC1',
+            d200: '#12181B',
+            d400: '#FFFFFF',
+            d700: '#080C0D',
+        },
+        blue: {
+            100: '#6CB4EE',
+            200: '#2a52be',
+            250: '#0066b2',
+            300: '#0039a6',
+            350: '#034694',
+            400: '#00308F',
+            500: '#002D62',
+            600: '#002244',
+        },
+        black: {
+            50: '#36454F',
+            100: '#353935',
+            200: '#343434',
+            300: '#28282B',
+            400: '#000000',
+        },
+        brown: {
+            20: '#F6F5F2',
+            100: 'B79E8F',
+            300: '604F44',
+            400: '3A2B22',
+        },
+        palette_purple: '#736CED',
+        palette_indigo: '#9F9FED',
+        palette_lavender: '#D4C1EC',
+        palette_pink: '#FFC0CB',
+        palette_blue: '#BDE0FE',
     },
-    blue: {
-      100: '#6CB4EE',
-      200: '#2a52be',
-      250: '#0066b2',
-      300: '#0039a6',
-      350: '#034694',
-      400: '#00308F',
-      500: '#002D62',
-      600: '#002244',
+    gradients: {
+        palette_purple_gradient: `linear-gradient(to right top, #736ced, #7b76ee, #837fee, #8c89ee, #9492ee, #969df1, #99a7f3, #9eb1f5, #a1bef9, #a7cafb, #b0d5fd, #bde0fe)`,
     },
-    black: {
-      50: '#36454F',
-      100: '#353935',
-      200: '#343434',
-      300: '#28282B',
-      400: '#000000',
+    fonts: {
+        body: 'reddit-mono, sans-serif',
+        heading: 'reddit-mono, sans-serif',
     },
-    brown: {
-      20: '#F6F5F2',
-      100: 'B79E8F',
-      300: '604F44',
-      400: '3A2B22',
+    components: {
+        Heading: {
+            baseStyle: {
+                fontFamily: 'inherit',
+                fontWeight: 'semibold',
+                color: 'inherit',
+                fontSize: 'lg',
+            },
+        },
+        Text: {
+            baseStyle: {
+                fontFamily: 'inherit',
+                fontWeight: 'normal',
+                lineHeight: 'tall',
+                color: 'inherit',
+            },
+        },
+        Button: {
+            baseStyle: {
+                fontWeight: 'normal',
+                userSelect: 'none',
+            },
+        },
+        Avatar: {
+            icon: FaUserCircle,
+        },
     },
-    palette_purple: '#736CED',
-    palette_indigo: '#9F9FED',
-    palette_lavender: '#D4C1EC',
-    palette_pink: '#FFC0CB',
-    palette_blue: '#BDE0FE',
-  },
-  fonts: {
-    body: 'reddit-mono, sans-serif',
-    heading: 'reddit-mono, sans-serif',
-  },
-  components: {
-    Heading: {
-      baseStyle: {
-        fontFamily: 'inherit',
-        fontWeight: 'semibold',
-        color: 'inherit',
-        fontSize: 'lg',
-      },
-    },
-    Text: {
-      baseStyle: {
-        fontFamily: 'inherit',
-        fontWeight: 'normal',
-        lineHeight: 'tall',
-        color: 'inherit',
-      },
-    },
-    Button: {
-      baseStyle: {
-        fontWeight: 'normal',
-        userSelect: 'none',
-      },
-    },
-    Avatar: {
-      icon: FaUserCircle,
-    },
-  },
 };
 
 export default theme;
