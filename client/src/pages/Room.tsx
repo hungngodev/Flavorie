0be57ef4fc7144b671d5b04777a3371400fdc2e3
HUNG NGO <hungngo1607@gmail.com>
2024-07-04 01:24:48 +0700

    adding slide to call

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -1,4 +1,17 @@
-import { Button, Card, CardBody, Grid, GridItem, HStack, IconButton, useDisclosure } from '@chakra-ui/react';
+import {
+    Box,
+    Button,
+    Card,
+    CardBody,
+    Grid,
+    GridItem,
+    HStack,
+    IconButton,
+    VStack,
+    useDisclosure,
+} from '@chakra-ui/react';
+import { useQuery } from '@tanstack/react-query';
+import { ChakraStylesConfig, Select } from 'chakra-react-select';
 import { motion } from 'framer-motion';
 import {
     Clipboard,
@@ -13,10 +26,50 @@ import {
 } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import ImageSlide from '../components/meals/ImageSlide';
 import { Chat, NameInput, VideoPlayer } from '../components/meeting';
 import { useRoom, useUser } from '../hooks';
 import { ws } from '../providers/RoomProvider';
 import { PeerState } from '../reducers/peerReducer';
+import customFetch from '../utils/customFetch';
+
+const menuStyles: ChakraStylesConfig = {
+    container: (baseStyles, state) => ({
+        ...baseStyles,
+        width: '30%',
+
+        border: 'none',
+    }),
+    control: (baseStyles, state) => ({
+        ...baseStyles,
+        border: 'none',
+    }),
+    indicatorsContainer: (baseStyles, state) => ({
+        backgroundColor: 'transparent',
+        background: 'transparent',
+    }),
+    indicatorSeparator: (baseStyles, state) => ({
+        backgroundColor: 'gray.600',
+    }),
+    dropdownIndicator: (baseStyles, state) => ({
+        backgroundColor: 'transparent',
+    }),
+    valueContainer: (baseStyles, state) => ({
+        ...baseStyles,
+        border: 'none',
+        paddingInline: '0',
+        fontSize: 'xl',
+        color: 'blackAlpha.700',
+    }),
+};
+
+const likedMealQuery = {
+    queryKey: ['likedMeal'],
+    queryFn: async () => {
+        const response = await customFetch.get('/user/likedMeal');
+        return response;
+    },
+};
 
 const Room = () => {
     const { id } = useParams();
@@ -35,6 +88,18 @@ const Room = () => {
     } = useRoom();
     const { userName, userId } = useUser();
     const [focus, setFocus] = useState(screenSharingId);
+
+    const { data: likedMeal, status } = useQuery(likedMealQuery);
+    const mealSuggestions =
+        status !== 'pending' ? likedMeal?.data?.likedMeals?.map((meal: any) => meal.likedMeal.title) : [''];
+    console.log(likedMeal);
+    const menuSuggestion = {
+        label: 'Suggested Menu',
+        options: mealSuggestions,
+    };
+    const [mealChoice, setMealChoice] = useState(status !== 'pending' ? mealSuggestions[0] : []);
+    console.log(mealSuggestions);
+    console.log(mealChoice);
 
     useEffect(() => {
         setFocus(screenSharingId);
@@ -72,45 +137,85 @@ const Room = () => {
         >
             <GridItem rowSpan={13} colSpan={1}>
                 <HStack height="100%" width="100%" alignItems={'start'} padding={'5px'}>
-                    <Grid height={'100%'} width="100%" templateColumns={`repeat(5,1fr)`} templateRows={`repeat(3,1fr)`}>
-                        {focusingVideo && (
-                            <GridItem rowSpan={3} colSpan={4} padding={'8px'} onClick={() => setFocus('')}>
-                                <Card width={'full'} height="full" display={'flex'} justify={'center'} align={'center'}>
-                                    <VideoPlayer stream={focusingVideo} />
-                                </Card>
-                            </GridItem>
-                        )}
+                    <VStack>
+                        <Grid
+                            height={'80%'}
+                            width="100%"
+                            templateColumns={`repeat(5,1fr)`}
+                            templateRows={`repeat(2,1fr)`}
+                        >
+                            {focusingVideo && (
+                                <GridItem rowSpan={3} colSpan={4} padding={'8px'} onClick={() => setFocus('')}>
+                                    <Card
+                                        width={'full'}
+                                        height="full"
+                                        display={'flex'}
+                                        justify={'center'}
+                                        align={'center'}
+                                    >
+                                        <VideoPlayer stream={focusingVideo} />
+                                    </Card>
+                                </GridItem>
+                            )}
 
-                        {focus !== userId && (
-                            <GridItem rowSpan={1} colSpan={1} padding={'8px'} onClick={() => setFocus(userId)}>
-                                <Card width={'full'} height="full" display={'flex'} justify={'center'} align={'center'}>
-                                    <CardBody>
-                                        <VideoPlayer stream={stream} />
-                                        <NameInput />
-                                    </CardBody>
-                                </Card>
-                            </GridItem>
-                        )}
-                        {peerVideos
-                            .filter((peer) => !!peer.stream)
-                            .map((peer, index) => (
-                                <GridItem
-                                    key={index}
-                                    rowSpan={1}
-                                    colSpan={1}
-                                    padding={'8px'}
-                                    onClick={() => setFocus(peer.userId)}
-                                >
-                                    <Card width={'full'} height="full">
+                            {focus !== userId && (
+                                <GridItem rowSpan={1} colSpan={1} padding={'8px'} onClick={() => setFocus(userId)}>
+                                    <Card
+                                        width={'full'}
+                                        height="full"
+                                        display={'flex'}
+                                        justify={'center'}
+                                        align={'center'}
+                                    >
                                         <CardBody>
-                                            <VideoPlayer stream={peer.stream} />
-                                            <div>{peer.userName}</div>
+                                            <VideoPlayer stream={stream} />
+                                            <NameInput />
                                         </CardBody>
                                     </Card>
                                 </GridItem>
-                            ))}
-                    </Grid>
-
+                            )}
+                            {peerVideos
+                                .filter((peer) => !!peer.stream)
+                                .map((peer, index) => (
+                                    <GridItem
+                                        key={index}
+                                        rowSpan={1}
+                                        colSpan={1}
+                                        padding={'8px'}
+                                        onClick={() => setFocus(peer.userId)}
+                                    >
+                                        <Card width={'full'} height="full">
+                                            <CardBody>
+                                                <VideoPlayer stream={peer.stream} />
+                                                <div>{peer.userName}</div>
+                                            </CardBody>
+                                        </Card>
+                                    </GridItem>
+                                ))}
+                        </Grid>
+                        <HStack width={'full'} height="full">
+                            <Select
+                                chakraStyles={menuStyles}
+                                defaultValue={mealSuggestions[0]}
+                                value={mealChoice}
+                                options={[menuSuggestion]}
+                                onChange={(newValue: any) => {
+                                    setMealChoice(newValue);
+                                }}
+                            />
+                            {status !== 'pending' && mealSuggestions.some((e) => e === mealChoice) && (
+                                <Box width="40vh" height="full" mx="auto" px={4}>
+                                    <ImageSlide
+                                        backendData={
+                                            likedMeal?.data?.likedMeals?.find(
+                                                (meal: any) => meal.likedMeal.title === mealChoice,
+                                            )?.likedMeal
+                                        }
+                                    />
+                                </Box>
+                            )}
+                        </HStack>
+                    </VStack>
                     <motion.div
                         {...getDisclosureProps()}
                         hidden={hidden}
